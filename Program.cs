762912837688098ef112a8728b508f9bd6b97232
alexis.desgrange <alexis.desgrange@diiage.org>
2023-10-23 14:18:42 +0200

    API Ocelot avec route pour pod micro service auth

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false);
+
+builder.Services.AddOcelot();
+builder.Services.AddCors();
+builder.Services.AddMvc();
+
+
+var app = builder.Build();
+
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+
+app.UseOcelot();
+
+app.Run();
